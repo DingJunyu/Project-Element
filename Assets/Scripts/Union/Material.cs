b6a0b89d8b14f85cc83144ac0a596bc47735889c
DING JUNYU <dd196430270@gmail.com>
2019-06-27 15:00:34 +0900

    6.27 2
    -攻撃の方式を定義しました。

--- Assets/Scripts/Union/Material.cs
+++ Assets/Scripts/Union/Material.cs
@@ -6,18 +6,31 @@ public class Material : MonoBehaviour
 {
     public bool typeRandom;
 
-    public  int capacity;
+    /*属性ダメージ部分*/
+    public int capacity;
     private int thisType;
     public int ReferThisType() { return thisType; }
-    public  float Damage;
+    public float Damage;
+
+    /*ダメージ形部分*/
+    private int shapeName;
+    private int stepName;
+    private int strength;
+    private float speed;
+    private bool fog = false;
 
     public bool choosed;
 
     public int setMyType;
     public string name;
 
-    private void Start()
-    {
+    enum randomStandard {
+        better,
+        medium,
+        weak
+    }
+
+    private void Start() {
         capacity = Random.Range(15, 50);
         Damage = (float)capacity * Random.Range(0f,1.6f);
 
@@ -27,5 +40,149 @@ public class Material : MonoBehaviour
             thisType = Random.Range(0,5);
         else
             thisType = setMyType;
+
+        SetShapeAndType();//属性に基づいてデータを生成する
+    }
+
+    void SetOneShape(int ranA,int ranB,
+        int volA,int volB,int volC) {
+        int randNum = Random.Range(0, 100);
+        /*ここの数字はすべて確率であり*/
+        if (randNum < ranA) {
+            SetShapeName((int)TypeOfAttack.Shape.ShapeType.rayOfLight,
+                volA);
+        }
+        else if (randNum < ranB) {
+            SetShapeName((int)TypeOfAttack.Shape.ShapeType.radialLight,
+                volB);
+        }
+        else {
+            SetShapeName((int)TypeOfAttack.Shape.ShapeType.annulus,
+                volC);
+        }
+    }
+
+    void SetShapeName(int str,int randSta) {
+        int ranLe = 0;
+        int ranRi = 5;
+
+        switch (randSta) {
+            case (int)randomStandard.better: ranLe = 12; ranRi = 16; break;
+            case (int)randomStandard.medium: ranLe = 8; ranRi = 12; break;
+            case (int)randomStandard.weak: ranLe = 4; ranRi = 8; break;
+        }
+
+        shapeName = str;
+        strength = Random.Range(ranLe, ranRi);
+    }
+
+    void SetOneStep(int ranA, int ranB,
+       int volA, int volB, int volC) {
+        int randNum = Random.Range(0, 100);
+        /*ここの数字はすべて確率であり*/
+        if (randNum < ranA) {
+            SetTypeName((int)TypeOfAttack.Shape.ShapeType.rayOfLight,
+                volA);
+        }
+        else if (randNum < ranB) {
+            SetTypeName((int)TypeOfAttack.Shape.ShapeType.radialLight,
+                volB);
+        }
+        else {
+            SetTypeName((int)TypeOfAttack.Shape.ShapeType.annulus,
+                volC);
+        }
+    }
+
+
+    void SetTypeName(int str, int randSta) {
+        int ranLe = 0;
+        int ranRi = 5;
+
+        switch (randSta) {
+            case (int)randomStandard.better: ranLe = 12; ranRi = 16; break;
+            case (int)randomStandard.medium: ranLe = 8; ranRi = 12; break;
+            case (int)randomStandard.weak: ranLe = 4; ranRi = 8; break;
+        }
+
+        stepName = str;
+        strength = Random.Range(ranLe, ranRi);
+    }
+
+    private void SetShapeAndType() {
+        switch (thisType) {
+            case (int)TypeOfMagic.Type.water: ANewWaterMaterial(); break;
+            case (int)TypeOfMagic.Type.wood: ANewWoodMaterial(); break;
+            case (int)TypeOfMagic.Type.fire: ANewFireMaterial(); break;
+            case (int)TypeOfMagic.Type.metal: ANewMetalMaterial(); break;
+            case (int)TypeOfMagic.Type.earth: ANewEarthMaterial(); break;
+        }
+    }
+
+    private void ANewWaterMaterial() {//水タイプの初期化
+        speed = Random.Range(0.8f, 1.4f);
+
+        SetOneShape(20, 40, (int)randomStandard.weak,
+             (int)randomStandard.medium,
+             (int)randomStandard.better);
+        SetOneStep(10, 30, (int)randomStandard.weak,
+             (int)randomStandard.medium,
+             (int)randomStandard.better);
+
+        if (Random.Range(0, 100) > 40)
+            fog = true;
+    }
+
+    private void ANewWoodMaterial() {//木タイプの初期化
+        speed = Random.Range(0.8f, 1.4f);
+
+        SetOneShape(50, 90, (int)randomStandard.better,
+             (int)randomStandard.medium,
+             (int)randomStandard.weak);
+        SetOneStep(70, 90, (int)randomStandard.better,
+             (int)randomStandard.weak,
+             (int)randomStandard.weak);
+    
+    }
+
+    private void ANewFireMaterial() {//火タイプの初期化
+        speed = Random.Range(0.8f, 1.4f);
+
+        SetOneShape(20, 80, (int)randomStandard.weak,
+           (int)randomStandard.better,
+           (int)randomStandard.weak);
+        SetOneStep(80, 100, (int)randomStandard.better,
+             (int)randomStandard.weak,
+             (int)randomStandard.weak);
+
+        if (Random.Range(0, 100) > 80)
+            fog = true;
+    }
+
+    private void ANewMetalMaterial() {//金タイプの初期化
+        speed = Random.Range(0.8f, 1.4f);
+
+        SetOneShape(60, 90, (int)randomStandard.better,
+           (int)randomStandard.medium,
+           (int)randomStandard.weak);
+        SetOneStep(80, 100, (int)randomStandard.better,
+             (int)randomStandard.weak,
+             (int)randomStandard.weak);
+    }
+
+    private void ANewEarthMaterial() {//土タイプの初期化
+        speed = Random.Range(0.8f, 1.4f);
+
+        SetOneShape(30, 60, (int)randomStandard.medium,
+           (int)randomStandard.medium,
+           (int)randomStandard.better);
+        SetOneStep(30, 90, (int)randomStandard.medium,
+             (int)randomStandard.better,
+             (int)randomStandard.weak);
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log(name);
     }
 }
--- Assets/Scripts/Union/TypeOfAttack.cs
+++ Assets/Scripts/Union/TypeOfAttack.cs
@@ -4,5 +4,122 @@ using UnityEngine;
 
 public class TypeOfAttack
 {
+    public class Shape {
+        public enum ShapeType {
+            rayOfLight,
+            radialLight,
+            annulus
+        }
 
+        int rayOfLight;
+        int radialLight;
+        int annulus;
+
+        int maxOne;
+        int maxName;
+
+        public int RefermaxName() { return maxName; }
+
+        public Shape() {
+            rayOfLight = 1;
+            radialLight = 0;
+            annulus = 0;
+
+            maxOne = rayOfLight;
+            maxName = (int)ShapeType.rayOfLight;
+        }
+
+        //新しいものを入れる、最大値を更新する
+        public bool inputNew(int thisType, int cap) {
+            switch (thisType) {
+                case (int)ShapeType.rayOfLight: rayOfLight += cap;
+                    if (rayOfLight > maxOne) {
+                        maxOne = rayOfLight;
+                        maxName = (int)ShapeType.rayOfLight;
+                    }
+                    break;
+                case (int)ShapeType.radialLight: radialLight += cap;
+                    if (radialLight > maxOne) {
+                        maxOne = radialLight;
+                        maxName = (int)ShapeType.radialLight;
+                    }
+                    break;
+                case (int)ShapeType.annulus: annulus += cap;
+                    if (annulus > maxOne) {
+                        maxOne = annulus;
+                        maxName = (int)ShapeType.annulus;
+                    } break;
+                default: return false;
+            }
+            return true;
+        }
+    }
+
+    public class SpeedAndSteps {
+        public enum TypeOfStep {
+            moment,
+            stage,
+            diffusion
+        }
+
+        int moment;//瞬間
+        int stage;//パー　パー　パーな感じ～
+        int diffusion;//拡散
+
+        float speed;
+
+        int maxOne;
+        int maxName;
+
+        public SpeedAndSteps() {
+            moment = 1;
+            stage = 0;
+            diffusion = 0;
+
+            maxOne = 1;
+            maxName = (int)TypeOfStep.moment;
+        }
+
+        public bool inputNew(int str, int cap, float spe) {
+            switch (str) {
+                case (int)TypeOfStep.moment: moment += cap;
+                    if (moment > maxOne) {
+                        maxOne = moment;
+                        maxName = (int)TypeOfStep.moment;
+                    }
+                    break;
+                case (int)TypeOfStep.stage:
+                    stage += cap;
+                    if (stage > maxOne)
+                    {
+                        maxOne = stage;
+                        maxName = (int)TypeOfStep.stage;
+                    }
+                    break;
+                case (int)TypeOfStep.diffusion:
+                    diffusion += cap;
+                    if (diffusion > maxOne)
+                    {
+                        maxOne = diffusion;
+                        maxName = (int)TypeOfStep.diffusion;
+                    }
+                    break;
+            }
+            return true;
+        }
+    }
+
+    bool isFog = false;//fogの材料を入れると一定確率でfogになれる
+    SpeedAndSteps speedAndSteps;
+    Shape shape;
+    int time;
+
+    public TypeOfAttack() {
+        speedAndSteps = new SpeedAndSteps();
+        shape = new Shape();
+    }
+
+    public void InputNewMaterials(Material material){
+
+    }
 }
