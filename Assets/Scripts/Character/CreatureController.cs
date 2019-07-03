5a49337b7ce1e574a6ee68e828c15b81e8a924e1
DING JUNYU <dd196430270@gmail.com>
2019-07-03 16:18:55 +0900

    7.03
    -アイテムとのあたり判定ができました

--- Assets/Scripts/Character/CreatureController.cs
+++ Assets/Scripts/Character/CreatureController.cs
@@ -7,24 +7,13 @@ public class CreatureController : MonoBehaviour
     private Vector2 speed;
     private Vector2 maxSpeed;
 
-    Rigidbody2D thisBody;
-
-    protected void MoveOnX(bool right,float newSpeed) {
-        if (right)
-            speed.x += newSpeed;
-        else
-            speed.x -= newSpeed;
-    }
-
     protected void Jump(float strength) {
 
     }
 
     // Start is called before the first frame update
     void Start() {
-        maxSpeed = new Vector2(20f, 0f);
 
-        thisBody = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -37,8 +26,6 @@ public class CreatureController : MonoBehaviour
     }
 
     private void CheckSpeed() {
-        if (thisBody.velocity.x >= maxSpeed.x) {
-            thisBody.velocity = new Vector2(maxSpeed.x, thisBody.velocity.y);
-        }
+
     }
 }
---
+++ Assets/Scripts/Character/MyCharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : CreatureController
+{
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GetOrder();
+    }
+
+    private void GetOrder() {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position = new Vector3(transform.position.x + .08f,
+                transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position = new Vector3(transform.position.x - .08f,
+                transform.position.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            Debug.Log("102");
+    }
+}
